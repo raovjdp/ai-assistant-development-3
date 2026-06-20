3abe7a828ccb71808e4fd406c090b19133c5d251
raovjdp <raorkoms@gmail.com>
2026-06-20 10:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,6 @@ def save_feedback():
     return jsonify({'status': 'feedback_saved'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
